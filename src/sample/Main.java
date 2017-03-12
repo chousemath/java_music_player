d537ea67443c3ce26f9392db4eeaf94b0a0903f0
Joseph Sungpil Choi <chousemath@gmail.com>
2017-03-13 01:16:20 +0900

    basically got back to where I was with the previous repo

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,17 +2,28 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        Text sceneTitle = new Text("Welcome Joseph");
+        sceneTitle.setFont(Font.font("Tahoma", FontWeight.BOLD, 20));
+        grid.add(sceneTitle, 0, 0, 2, 1);
+
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(grid, 300, 275));
         primaryStage.show();
     }
 
